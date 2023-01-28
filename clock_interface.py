0a45ea1854d0b652c617e3c9e625c8524aa84f44
lab630 <michaelhaneman.frc@gmail.com>
2023-01-27 16:16:16 -0800

    basic stabalization test

--- clock_interface.py
+++ clock_interface.py
@@ -6,16 +6,15 @@ class ClockInterface:
         self.freq_inst = freq_inst
         self.intensity_inst = intensity_inst
 
-    def get_res_freq(intensity_data, freq_data):
-        min_value = min(intensity_data)
-        min_index=freq_data.index(min_value)
-        return min_index
+    def get_res_freq(self, intensity_data, freq_data):
+        min_intensity = min(intensity_data)
+        min_intensity_index = intensity_data.index(min_intensity)
+        return freq_data[min_intensity_index]
 
     def res_freq_scan(self, base_freq, freq_low, freq_high):
         freq_data = []
         intensity_data = []
 
-        time.sleep(10)
         for f in range(freq_low, freq_high, 1):
             self.freq_inst.write('FREQ', f)
             intensity = self.intensity_inst.query('OUTP?3')
@@ -23,5 +22,5 @@ class ClockInterface:
             intensity_data.append(float(intensity))
             # print(f - base_freq, float(intensity))
 
-        plots.plot_points(x_data=freq_data, y_data=intensity_data) 
-        return 0
+        # plots.plot_points(x_data=freq_data, y_data=intensity_data)
+        return self.get_res_freq(intensity_data[2:], freq_data[2:])
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import time
-
-
 from hardware_io import Instr
 from clock_interface import ClockInterface
 
+
+# temp imports
+import matplotlib.pyplot as plt
+
+
 ''' <--- global variables --> '''
 base_freq = 6_834_682_610
-freq_low = base_freq + 7700
-freq_high = base_freq + 8600
-# freq_low = base_freq + 8050
-# freq_high = base_freq + 8350
+# freq_low = base_freq + 7700
+# freq_high = base_freq + 8600
+freq_low = base_freq + 8150
+freq_high = base_freq + 8300
 
 
 ''' <--- initalize devices ---> '''
@@ -33,16 +36,37 @@ SG386.write_and_verify('SFNC', 0)
 SG386.write_and_verify('MODL', 1) # make sure to turn on the modulation last
 SG386.write_and_verify('AMPH', -10)
 
+time.sleep(3)
+
 ''' <--- collect data ---> '''
 clock_interface = ClockInterface(freq_inst=SG386, intensity_inst=SR830)
 
-start_t = time.time()
-res_freq = clock_interface.res_freq_scan(
-    base_freq=base_freq, 
-    freq_low=freq_low, 
-    freq_high=freq_high)
 
-print("scanning time: ", time.time() - start_t)
+start_t = time.time()
+res_freqs = []
+res_times = []
+for n in range(10):
+    s = clock_interface.res_freq_scan(
+        base_freq=base_freq, 
+        freq_low=freq_low, 
+        freq_high=freq_high)
+
+    res_freqs.append(s)
+    res_times.append(time.time() - start_t) 
+    print("finished scan: ", n)
+
+
+print(res_freqs)
+
+plt.scatter(res_times, res_freqs)
+for i in range(len(res_freqs)):
+    plt.annotate(res_freqs[i], (res_times[i], res_freqs[i]))
+
+plt.xlabel("ellapsed time [S]")
+plt.ylabel("detuning frequency [Hz]")
+plt.show()
+
+
 
 ''' <--- terminate devices ---> '''
 SG386.write_and_verify('ENBH', 0) # turn off microwaves at the end of scanning?

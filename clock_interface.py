75af36ab98ff6384a52eccc0e36854b7305b3334
lab630 <michaelhaneman.frc@gmail.com>
2023-01-31 16:54:48 -0800

    updated UI and refactored scanning

--- clock_interface.py
+++ clock_interface.py
@@ -1,26 +1,59 @@
-import plots
 import time
+from tqdm import tqdm
+
+from plots import PlotObject
+import modeling
 
 class ClockInterface:
     def __init__(self, freq_inst, intensity_inst):
         self.freq_inst = freq_inst
         self.intensity_inst = intensity_inst
 
-    def get_res_freq(self, intensity_data, freq_data):
-        min_intensity = min(intensity_data)
-        min_intensity_index = intensity_data.index(min_intensity)
-        return freq_data[min_intensity_index]
 
-    def res_freq_scan(self, base_freq, freq_low, freq_high):
+    def res_freq_scan(self, freq_base, freq_low, freq_high):
         freq_data = []
         intensity_data = []
-
-        for f in range(freq_low, freq_high, 1):
+    
+        for f in tqdm(range(freq_low, freq_high, 1), desc="scanning clock..."):
             self.freq_inst.write('FREQ', f)
-            intensity = self.intensity_inst.query('OUTP?3')
-            freq_data.append(f - base_freq)
-            intensity_data.append(float(intensity))
-            # print(f - base_freq, float(intensity))
+            measured_intensity = self.intensity_inst.query('OUTP?3')
+            freq_data.append(f - freq_base)
+            intensity_data.append(float(measured_intensity))
 
-        # plots.plot_points(x_data=freq_data, y_data=intensity_data)
-        return self.get_res_freq(intensity_data[2:], freq_data[2:])
+        fit_y = modeling.gauss_fit(
+            x_data=freq_data,
+            y_data=intensity_data)
+
+        print(fit_y)
+
+        scan_plot = PlotObject(
+            x_label="detune freq [Hz]", 
+            y_label="Intensity")
+        scan_plot.plot_line(x_data=freq_data, y_data=intensity_data, label="measured")
+        scan_plot.plot_line(x_data=freq_data, y_data=fit_y, label="guass fit")
+        scan_plot.show_plot()
+
+        res_freq, res_intensity = modeling.find_min_val(freq_data[2:], intensity_data[2:])
+        print("res freq found: ", res_freq)
+        return res_freq
+
+
+    def cont_res_freq_scan(self, freq_base, freq_low, freq_high):
+        start_t = time.time()
+        res_freqs = []
+        res_times = []
+        for n in tqdm(range(10), desc="scanning..."):
+            single_scan = self.res_freq_scan(
+                freq_base=freq_base, 
+                freq_low=freq_low, 
+                freq_high=freq_high)
+
+            res_freqs.append(single_scan)
+            res_times.append(time.time() - start_t) 
+
+        cont_scan_plot = PlotObject(
+            x_label="ellapsed time [S]",
+            y_label="detuning frequency [Hz]",
+            show_annotate=True)
+
+        cont_scan_plot.plot_points(res_times, res_freqs)
--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from hardware_io import Instr
 from clock_interface import ClockInterface
 
 
-# temp imports
-import matplotlib.pyplot as plt
+''' ~~~~~~~~~~ GLOBAL SETTINGS ~~~~~~~~~~ '''
+MOCKING = False  # use data from mock file to simulate experiment
 
 
-''' <--- global variables --> '''
-base_freq = 6_834_682_610
+''' ~~~~~~~~~~ EXPERIMENT VARIABLES  ~~~~~~~~~~ '''
+
+freq_base = 6_834_682_610
 # freq_low = base_freq + 7700
 # freq_high = base_freq + 8600
-freq_low = base_freq + 8150
-freq_high = base_freq + 8300
+freq_low = freq_base + 8150
+freq_high = freq_base + 8300
 
 
 ''' <--- initalize devices ---> '''
 SR830 = Instr('GPIB0::8::INSTR', 9600)
-# SR830.write_and_verify('OUTX', 1)
 # SR830.query_and_print('*IDN?')
 SR830.write_and_verify('SENS', 8)
 SR830.write_and_verify('ILIN', 3)
@@ -33,39 +33,18 @@ SG386.write_and_verify('ENBH', 1)
 SG386.write_and_verify('TYPE', 3)
 SG386.write_and_verify('SRAT', 10)
 SG386.write_and_verify('SFNC', 0)
-SG386.write_and_verify('MODL', 1) # make sure to turn on the modulation last
+SG386.write_and_verify('MODL', 1) # configure modulation before turned on
 SG386.write_and_verify('AMPH', -10)
 
+
+''' <--- data collection ---> '''
+clock_interface = ClockInterface(freq_inst=SG386, intensity_inst=SR830)
 time.sleep(3)
 
-''' <--- collect data ---> '''
-clock_interface = ClockInterface(freq_inst=SG386, intensity_inst=SR830)
-
-
-start_t = time.time()
-res_freqs = []
-res_times = []
-for n in range(10):
-    s = clock_interface.res_freq_scan(
-        base_freq=base_freq, 
-        freq_low=freq_low, 
-        freq_high=freq_high)
-
-    res_freqs.append(s)
-    res_times.append(time.time() - start_t) 
-    print("finished scan: ", n)
-
-
-print(res_freqs)
-
-plt.scatter(res_times, res_freqs)
-for i in range(len(res_freqs)):
-    plt.annotate(res_freqs[i], (res_times[i], res_freqs[i]))
-
-plt.xlabel("ellapsed time [S]")
-plt.ylabel("detuning frequency [Hz]")
-plt.show()
-
+clock_interface.res_freq_scan(
+    freq_base=freq_base,
+    freq_low=freq_low,
+    freq_high=freq_high)
 
 
 ''' <--- terminate devices ---> '''
